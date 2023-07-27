abca4d489c40d1aba9c28921ccdbb1cbea3a4ed4
eiibrunoferreira <brunoferreirabf2002@gmail.com>
2023-07-27 11:36:52 -0300

    Validação tela de login e criação da tela home

--- js/login-script.js
+++ js/login-script.js
@@ -1,3 +1,36 @@
+const form = document.getElementById('form');
+const inputemail = document.getElementById('input-email');
+const emailvalidate = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+const inputpassword = document.getElementById('input-password');
+const btnlogin = document.getElementById('btn-to-enter');
+
+function logar() {
+
+    if(inputemail.value === "teste@gmail.com" && inputpassword.value === "teste123") {
+        location.href = "home.html";
+    } else if (inputemail.value === "" || inputpassword.value === "" ) {
+        alert ("Preencha todos os campos vazios!")
+    } else {
+        emailvalidatee();
+        passwordvalidate();
+    }
+}
+
+function emailvalidatee() {
+    if(inputemail.value === "teste@gmail.com") {
+        return true;
+    } else {
+        alert ('Email inválido');
+    }
+}
+
+function passwordvalidate() {
+    if(inputpassword.value === "teste123") {
+        return true;
+    } else {
+        alert ('Senha incorreta')
+    }
+}
 
 
 
